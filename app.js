b958ac36ba7226deca3ec020a6a54774507ffbeb
Tshering Nidup <ceering.nidup.7@gmail.com>
2024-11-28 10:12:52 +0600

    Add type writing effect to the Name

--- app.js
+++ app.js
@@ -4,3 +4,21 @@
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
   gtag('config', 'UA-XXXXXXXXX-X');
+
+
+// Name typing intro
+const text = "Welcome to Tshering's Portfolio 😁";
+const typingSpeed = 100; // Adjust typing speed (ms)
+
+let i = 0;
+
+function typeWriter() {
+    if (i < text.length) {
+        document.getElementById("typewriter-text").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typeWriter, typingSpeed);
+    }
+}
+
+// Start typing effect when the page loads
+window.onload = typeWriter;
